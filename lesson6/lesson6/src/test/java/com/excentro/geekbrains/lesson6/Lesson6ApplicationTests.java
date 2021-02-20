86f95f85e646a372ab04a18148979bb9e89ac00a
Mikhail Mishyn <excentro@gmail.com>
2021-02-20 08:07:16 +0300

    Empty Lesson 6

---
+++ lesson6/lesson6/src/test/java/com/excentro/geekbrains/lesson6/Lesson6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excentro.geekbrains.lesson6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
